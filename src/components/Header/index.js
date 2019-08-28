091ad9f1f09e4e3bea4198805308e44e723252af
Dealan <dealan.diongzon@gmail.com>
2019-08-27 22:52:32 -0400

    updated the .json file to have all the images as a link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 function Header() {
     return (
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Pokémon Clicky Game!</h1>
-                <p class="lead">Click on an image! Try not to click on any Pokémon more than once.</p>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Pokémon Clicky Game!</h1>
+                <p className="lead">Click on an image! Try not to click on any Pokémon more than once.</p>
             </div>
         </div>);
 }
