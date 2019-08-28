5adae8e2a1e216b32fa2cf52ffe0a3d00495ef40
Dealan <dealan.diongzon@gmail.com>
2019-08-27 21:23:17 -0400

    added main components of the page and took code from class examples and starting to replace it piece by piece

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-dark">
+      <h1 className="title">Pokémon Clicky Game</h1>
+      <h1 className="score">Score: 0 | Top Score: 0</h1>
+    </nav>);
+}
+
+export default NavBar;
