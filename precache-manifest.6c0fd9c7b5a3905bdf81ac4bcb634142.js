ba92dcafc3f304b64e1a9d11e56ccea64125887a
Dealan <dealan.diongzon@gmail.com>
2019-08-31 18:56:39 -0400

    Updates

---
+++ precache-manifest.6c0fd9c7b5a3905bdf81ac4bcb634142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd1ffa79d2103e8a861d4958616f84c",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "e65a1bfac79d5901e78b",
+    "url": "/clicky/static/css/main.0e5b75de.chunk.css"
+  },
+  {
+    "revision": "a51f7a69db61a516767e",
+    "url": "/clicky/static/js/2.9fdd8471.chunk.js"
+  },
+  {
+    "revision": "e65a1bfac79d5901e78b",
+    "url": "/clicky/static/js/main.ba0b1320.chunk.js"
+  },
+  {
+    "revision": "9d378f700a36f547cf37",
+    "url": "/clicky/static/js/runtime~main.68383fad.js"
+  },
+  {
+    "revision": "d4332a2931d4f479da730c4b3574f97d",
+    "url": "/clicky/static/media/background.d4332a29.jpg"
+  }
+]);
--- src/components/score.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// The Math function component accepts a props argument
-function Score() {
-  return(
-  <h1>poop</h1>)
-}
-
-export default Score;
