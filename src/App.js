e3d9df13ea6b77a304dc2ceba33807b4f86c50df
Dealan <dealan.diongzon@gmail.com>
2019-08-31 17:46:02 -0400

    moved the setState of concatinating the clicked array to if they user guesses right if area

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Wrapper from "./components/Wrapper";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import pokemons from "./pokemon.json";
-import Score from "./components/score.js";
 
 class App extends Component {
   // set the initial state
@@ -12,16 +11,23 @@ class App extends Component {
     pokemons,
     clicked: [],
     score: 0,
-    topScore: 0,
-    gameEnded: false,
+    topScore: 0
 
   };
+  
+  // resets everything
+  reset = () => {
+    this.setState({
+      score: 0,
+      clicked: []
+    })
+  }
 
   // this is what happens when the user clicks on a picture
   userClick = event => {
     event.preventDefault();
 
-    console.log("guessed: " + this.state.clicked);
+    // console.log("guessed: " + this.state.clicked);
 
     // gets the alt attribute from the image and saves it to a variable
     const clickedOn = event.target.alt;
@@ -33,37 +39,24 @@ class App extends Component {
 
     // if its -1, it updates the score else it resets the score to 0 and click to blank
     if(inGuessed === -1){
-      this.setState({score: this.state.score + 1},
-        () => {
-          if(this.state.score === 8){
-            this.setState({gameEnded: true});
-          }})
+      this.setState({clicked: this.state.clicked.concat(clickedOn)})
+      this.setState({score: this.state.score + 1})
     } else{
-      this.setState({
-        score: 0,
-        clicked: []})
+      this.reset();
     }
 
-    this.setState({clicked: this.state.clicked.concat(clickedOn)})
-    console.log("guessed: " + this.state.clicked);
     
   }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
-    console.log(this.state.gameEnded);
-    let message;
-    if (this.state.gameEnded){
-      message = <Score/>
-    }
+    console.log("guessed: " + this.state.clicked);
     return (
       <>
-        <NavBar
-        gameEnded={this.state.gameEnded} 
+        <NavBar 
         score={this.state.score} 
         topScore={this.state.topScore}/>
         <Header />
-        {message}
         <Wrapper>
           {this.state.pokemons.map(pokemon => (
           <Picture
